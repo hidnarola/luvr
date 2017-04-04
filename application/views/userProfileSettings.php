b68fd27c9f6fc910de5967730df60ca4d784c6ae
hiren <hda@narola.email>
2017-04-04 17:33:40 +0530

    Set value for gender radio on set user profile page.

--- application/views/userProfileSettings.php
+++ application/views/userProfileSettings.php
@@ -25,46 +25,46 @@
         echo form_label('Gender:', 'gender');
         ?>
         <label class="radio-inline">
-            <?php echo form_radio('gender', 'male'); ?> Male
+            <?php echo form_radio(array("name" => 'gender', 'value' => 'male', 'checked' => set_radio('gender', 'male'))); ?> Male
         </label>
         <label class="radio-inline">
-            <?php echo form_radio('gender', 'female'); ?> Female
+            <?php echo form_radio(array("name" => 'gender', 'value' => 'female', 'checked' => set_radio('gender', 'female'))); ?> Female
         </label>
     </div>
     <div class="form-group">
         <?php
         echo form_label('Age:', 'age');
-        echo form_input(array("name" => "age", "class" => "form-control", "type" => "number", "min" => "1"));
+        echo form_input(array("name" => "age", "class" => "form-control", "type" => "number", "min" => "1", 'value' => set_value('age')));
         ?>
     </div>
     <div class="form-group">
         <?php
         echo form_label('One Liner:', 'one_liner');
-        echo form_input(array("name" => "one_liner", "class" => "form-control"));
+        echo form_input(array("name" => "one_liner", "class" => "form-control", 'value' => set_value('one_liner')));
         ?>
     </div>
     <div class="form-group">
         <?php
         echo form_label('Job|Work:', 'job');
-        echo form_input(array("name" => "work", "class" => "form-control"));
+        echo form_input(array("name" => "work", "class" => "form-control", 'value' => set_value('work')));
         ?>
     </div>
     <div class="form-group">
         <?php
         echo form_label('Education:', 'school');
-        echo form_input(array("name" => "school", "class" => "form-control"));
+        echo form_input(array("name" => "school", "class" => "form-control", 'value' => set_value('school')));
         ?>
     </div>
     <div class="form-group">
         <?php
         echo form_label('Location:', 'address');
-        echo form_input(array("name" => "address", "class" => "form-control"));
+        echo form_input(array("name" => "address", "class" => "form-control", 'value' => set_value('address')));
         ?>
     </div>
     <div class="form-group">
         <?php
         echo form_label('About Me:', 'bio');
-        echo form_textarea(array("name" => "bio", "class" => "form-control"));
+        echo form_textarea(array("name" => "bio", "class" => "form-control", 'value' => set_value('bio')));
         ?>
     </div>
     <?php
