057af35ee2ab95f9788e12bab9b8e5146680a7a2
hiren <hda@narola.email>
2017-04-04 10:13:05 +0530

    Fixed redirection issue.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,8 +1,8 @@
-<?php
+</div>
+</div>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<footer class="container-fluid text-center">
+    <p>Footer Text</p>
+</footer>
 
+</body>
