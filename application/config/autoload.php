ad3bdf7406e76eff0a4129dc400328a41d1d14e0
RDA <rda@narola.email>
2017-04-04 10:17:43 +0530

    new chanes

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
   |
   |	$autoload['helper'] = array('url', 'file');
  */
-$autoload['helper'] = array('site', 'url');
+$autoload['helper'] = array('site', 'url','file');
 
 /*
   | -------------------------------------------------------------------
