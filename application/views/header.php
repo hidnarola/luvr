f4cceebee806d27a431add49d489769d0bcec3e7
hiren <hda@narola.email>
2017-04-08 09:10:22 +0530

    Removed dummy menus.

--- application/views/header.php
+++ application/views/header.php
@@ -116,9 +116,8 @@ else
             </div>
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
+                    <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
                     <li><a href="#">About</a></li>
-                    <li><a href="#">Projects</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
