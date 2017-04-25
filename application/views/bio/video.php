c687f9341d6ed1b979dcae497902fe4f47387943
hiren <hda@narola.email>
2017-04-25 12:50:57 +0530

    Changed dummy id.

--- application/views/bio/video.php
+++ application/views/bio/video.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <?php if (!empty($video_url)) { ?>
-            <div id="myDiv"></div>        
+            <div id="playerObject"></div>        
         <?php } else { ?>
             <p class="alert alert-danger">Invalid Video URL!</p>
         <?php } ?>
@@ -11,7 +11,7 @@
 <?php if (!empty($video_url)) { ?>
     <script type="text/javascript" src="https://content.jwplatform.com/libraries/Xcqi8yCH.js"></script>
     <script type="text/javascript">
-        jwplayer("myDiv").setup({
+        jwplayer("playerObject").setup({
         "file": "<?php echo $video_url; ?>",
     <?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me') { ?>
             advertising: {
