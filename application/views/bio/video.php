b88f0dda0e863d772195365454ed20d7421e684b
hiren <hda@narola.email>
2017-04-26 09:58:46 +0530

    Configured jwplayer.

--- application/views/bio/video.php
+++ application/views/bio/video.php
@@ -7,14 +7,20 @@
         <?php } ?>
     </div>
 </div>
-
+<style type="text/css">
+    .jw-progress{background:#f26f6f;}
+    .jw-button-color:focus, :not(.jw-flag-touch) .jw-button-color:hover{color:#f26f6f;}
+</style>
 <?php if (!empty($video_url)) { ?>
-                <!--<script type="text/javascript" src="https://content.jwplatform.com/libraries/IZbDCxfW.js"></script>-->
     <script type="text/javascript" src="<?php echo base_url('assets/js/jwplayer.js'); ?>"></script>
     <script>jwplayer.key = "+NBpDYuEp+FQ1VZ4YR8hbrcC1s9O/eD5ul+RdSAMR04=";</script>
     <script type="text/javascript">
         jwplayer("playerObject").setup({
-        "file": "<?php echo $video_url; ?>",
+        file: "<?php echo $video_url; ?>",
+                autostart: true,
+                aspectratio:"16:9",
+                width: "100%",
+                preload:"metadata",
     <?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me') { ?>
             advertising: {
             client: 'vast',
