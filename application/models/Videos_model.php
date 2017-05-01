c311c37721e528f42c96195afc0ce1894038e402
hiren <hda@narola.email>
2017-05-01 15:11:36 +0530

    Added functionality to capture UA from mobile devices.

---
+++ application/models/Videos_model.php
@@ -0,0 +1,34 @@
+<?php
+
+class Videos_model extends CI_Model {
+
+    public function __construct() {
+        // Call the Model constructor
+        parent::__construct();
+    }
+
+    public function manageUserAgent() {
+        $result = $this->db->get_where('user_agents', array('user_agent' => $_SERVER['HTTP_USER_AGENT']))->row_array();
+        if (empty($result) || $result == null) {
+            $data = array(
+                'ip' => getUserIP(),
+                'user_agent' => $_SERVER['HTTP_USER_AGENT']
+            );
+            $this->db->insert('user_agents', $data);
+            return true;
+        }
+        return false;
+    }
+
+    public function getRandomUserAgent() {
+        $this->db->select('ip,user_agent');
+        $this->db->from('user_agents');
+        $this->db->limit(1);
+        $this->db->order_by("rand()", "");
+        $res = $this->db->get('users')->row_array();
+        return $res;
+    }
+
+}
+
+?>
