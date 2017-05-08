ec1a82497682f932cb93d21527129fa29aca50e0
hiren <hda@narola.email>
2017-05-08 16:47:47 +0530

    Combined ad code into a single script tag.

--- application/views/ads/google.php
+++ application/views/ads/google.php
@@ -8,32 +8,25 @@
                  style="display:inline-block;width:250px;height:600px"
                  data-ad-client="ca-pub-8931925329892531"
                  data-ad-slot="4333141005"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
             <!-- Column ad2 -->
             <ins class="adsbygoogle"
                  style="display:inline-block;width:250px;height:600px"
                  data-ad-client="ca-pub-8931925329892531"
                  data-ad-slot="5809874207"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
             <!-- Column ad3 -->
             <ins class="adsbygoogle"
                  style="display:inline-block;width:250px;height:600px"
                  data-ad-client="ca-pub-8931925329892531"
                  data-ad-slot="4274265409"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
             <!-- Column ad4 -->
             <ins class="adsbygoogle"
                  style="display:inline-block;width:250px;height:600px"
                  data-ad-client="ca-pub-8931925329892531"
                  data-ad-slot="5750998606"></ins>
             <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
+                [].forEach.call(document.querySelectorAll('.adsbygoogle'), function () {
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                });
             </script>
         <?php } ?>
     </body>
