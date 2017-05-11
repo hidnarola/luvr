2b1c0106d9f6c26a3bb6c9104836a8e5eca5f6bd
hiren <hda@narola.email>
2017-05-11 14:30:34 +0530

    Debugging.

--- application/views/ads/adcash.php
+++ application/views/ads/adcash.php
@@ -1,5 +1,5 @@
-<?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me' && $show_ad == true) { ?>
+<?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me') { ?>
     <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=1572461"></script>
-<?php } else if ($_SERVER['HTTP_HOST'] == 'luvr.me' && $show_ad == true) { ?>
+<?php } else if ($_SERVER['HTTP_HOST'] == 'luvr.me') { ?>
     <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=1575965"></script>
 <?php } ?>
