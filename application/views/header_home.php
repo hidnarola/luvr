42d4948d5b0ab0e3e8a8eda63ae21596106ad53b
hiren <hda@narola.email>
2017-05-11 12:26:03 +0530

    FIxed analytics issue.

--- application/views/header_home.php
+++ application/views/header_home.php
@@ -30,6 +30,23 @@ if (empty($user_data)) {
         $this->minify->js(array('jquery.min.js', 'bootstrap.min.js'));
         echo $this->minify->deploy_js(FALSE, 'combined.min.js');
         ?>
+        <?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me' || $_SERVER['HTTP_HOST'] == 'luvr.me') { ?>
+            <script>
+                (function (i, s, o, g, r, a, m) {
+                    i['GoogleAnalyticsObject'] = r;
+                    i[r] = i[r] || function () {
+                        (i[r].q = i[r].q || []).push(arguments)
+                    }, i[r].l = 1 * new Date();
+                    a = s.createElement(o),
+                            m = s.getElementsByTagName(o)[0];
+                    a.async = 1;
+                    a.src = g;
+                    m.parentNode.insertBefore(a, m)
+                })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+                ga('create', 'UA-87679442-2', 'auto');
+                ga('send', 'pageview');
+            </script>
+        <?php } ?>
     </head> 
     <body>
         <header id="header">
