d28c64cbd2da8f6f50ccbe9ad91f2af8be33c204
hiren <hda@narola.email>
2017-06-06 12:57:25 +0530

    Minor fix.

--- application/views/ads/adcash.php
+++ application/views/ads/adcash.php
@@ -1,5 +1,5 @@
 <?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me') { ?>
-    <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=1572461"></script>
+    <script data-cfasync="false" type="text/javascript" src="<?php echo $_SERVER['REQUEST_SCHEME']; ?>://www.tradeadexchange.com/a/display.php?r=1572461"></script>
 <?php } else if ($_SERVER['HTTP_HOST'] == 'luvr.me') { ?>
-    <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=1575965"></script>
+    <script data-cfasync="false" type="text/javascript" src="<?php echo $_SERVER['REQUEST_SCHEME']; ?>://www.tradeadexchange.com/a/display.php?r=1575965"></script>
 <?php } ?>
