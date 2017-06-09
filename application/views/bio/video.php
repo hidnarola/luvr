49fd49ad0940c20d6ba50ce6ebf456542bf55fe7
hiren <hda@narola.email>
2017-06-09 10:50:23 +0530

    Debugging

--- application/views/bio/video.php
+++ application/views/bio/video.php
@@ -28,6 +28,9 @@ if (isset($_GET['s']) && !empty($_GET['s'])) {
     $show_ad = true;
     echo "IP : " . $this->input->ip_address();
 }
+if (isset($_GET['p']) && !empty($_GET['p']) && (empty($_GET['s'])) || !isset($_GET['s'])) {
+    $playlist = null;
+}
 if (isset($ad_url) && !empty($ad_url)) {
     if (strpos($ad_url, 'streamrail') !== false) {
         $next_random_url = str_replace("https", "http", $next_random_url);
@@ -41,47 +44,45 @@ if (isset($ad_url) && !empty($ad_url)) {
 <div class="container">
     <div class="row">
         <div class="back-btn-div"><a onclick="window.history.back();" class="for_pointer"></a></div>    
-        <?php if (!empty($playlist)) { ?>
-            <div id="playerObject"></div>        
-        <?php } else { ?>
-            <p class="alert alert-danger">Invalid Video URL!</p>
-        <?php } ?>
+        <div id="playerObject"></div>        
     </div>
 </div>
 <style type="text/css">
     .jw-progress{background:#f26f6f;}
     .jw-button-color:focus, :not(.jw-flag-touch) .jw-button-color:hover{color:#f26f6f;}
 </style>
-<?php if (!empty($playlist)) { ?>
-    <?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me' && $show_ad == true) { ?>
-        <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=1572461"></script>
-    <?php } else if ($_SERVER['HTTP_HOST'] == 'luvr.me' && $show_ad == true) { ?>
-        <script data-cfasync="false" type="text/javascript" src="<?php echo $_SERVER['REQUEST_SCHEME']; ?>://www.tradeadexchange.com/a/display.php?r=1592351"></script>
-    <?php } ?>
-    <script type="text/javascript">
-                console.log('<?php echo count($playlist); ?>');
-                var player = jwplayer('playerObject');
-                player.setup({
+<?php if ($_SERVER['HTTP_HOST'] == 'dev.luvr.me' && $show_ad == true) { ?>
+    <script data-cfasync="false" type="text/javascript" src="http://www.tradeadexchange.com/a/display.php?r=1572461"></script>
+<?php } else if ($_SERVER['HTTP_HOST'] == 'luvr.me' && $show_ad == true) { ?>
+    <script data-cfasync="false" type="text/javascript" src="<?php echo $_SERVER['REQUEST_SCHEME']; ?>://www.tradeadexchange.com/a/display.php?r=1592351"></script>
+<?php } ?>
+<script type="text/javascript">
+            console.log('<?php echo count($playlist); ?>');
+            var player = jwplayer('playerObject');
+            player.setup({
+<?php if ($playlist != null && !empty($playlist)) { ?>
                 playlist: <?php echo json_encode($playlist); ?>,
-    <?php if ($show_ad == true) { ?>
-                    primary:'flash',
-    <?php } ?>
-    <?php if ($show_ad == false) { ?>
-                    repeat:true,
-                            autostart:false,
-    <?php } ?>
-    <?php if ($show_ad == true) { ?>
-                    autostart:true,
-    <?php } ?>
-                aspectratio:"16:9",
-                        width:"100%",
-    <?php if (($_SERVER['HTTP_HOST'] == 'dev.luvr.me' || $_SERVER['HTTP_HOST'] == 'luvr.me') && $show_ad == true) { ?>
-                    advertising: {
-                    client:'vast',
-                            tag:'<?php echo $ad_url; ?>',
-                    },
-    <?php } ?>
-                });
+<?php } ?>
+<?php if ($show_ad == true) { ?>
+                primary:'flash',
+<?php } ?>
+<?php if ($show_ad == false) { ?>
+                repeat:true,
+                        autostart:false,
+<?php } ?>
+<?php if ($show_ad == true) { ?>
+                autostart:true,
+<?php } ?>
+            aspectratio:"16:9",
+                    width:"100%",
+<?php if (($_SERVER['HTTP_HOST'] == 'dev.luvr.me' || $_SERVER['HTTP_HOST'] == 'luvr.me') && $show_ad == true) { ?>
+                advertising: {
+                client:'vast',
+                        tag:'<?php echo $ad_url; ?>',
+                },
+<?php } ?>
+            });
+<?php if ($playlist != null && !empty($playlist)) { ?>
                 player.on('error', function () {
                 var next = parseInt(jwplayer().getPlaylistIndex()) + 1;
                 if (next < <?php echo count($playlist); ?>) {
@@ -109,11 +110,11 @@ if (isset($ad_url) && !empty($ad_url)) {
                     location.href = '<?php echo $next_random_url; ?>';
                     });
     <?php } ?>
-    <?php if (!empty($ad_url) && $show_ad == true) { ?>
-                    console.log('<?php echo $ad_url; ?>');
-    <?php } ?>
-    <?php if (!empty($next_random_url) && $show_ad == true) { ?>
-                    console.log('<?php echo $next_random_url; ?>');
-    <?php } ?>
-    </script>
 <?php } ?>
+<?php if (!empty($ad_url) && $show_ad == true) { ?>
+                console.log('<?php echo $ad_url; ?>');
+<?php } ?>
+<?php if (!empty($next_random_url) && $show_ad == true) { ?>
+                console.log('<?php echo $next_random_url; ?>');
+<?php } ?>
+</script>
