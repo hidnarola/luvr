794d138613a4050208e173392e67f78f8afee83c
hiren <hda@narola.email>
2017-06-12 10:39:10 +0530

    Debugging

--- application/views/ads/testproxy.php
+++ application/views/ads/testproxy.php
@@ -1,4 +1,25 @@
 <?php
 
 pr($_REQUEST);
+$proxy_headers = array(
+    'HTTP_VIA',
+    'HTTP_X_FORWARDED_FOR',
+    'HTTP_FORWARDED_FOR',
+    'HTTP_X_FORWARDED',
+    'HTTP_FORWARDED',
+    'HTTP_CLIENT_IP',
+    'HTTP_FORWARDED_FOR_IP',
+    'VIA',
+    'X_FORWARDED_FOR',
+    'FORWARDED_FOR',
+    'X_FORWARDED',
+    'FORWARDED',
+    'CLIENT_IP',
+    'FORWARDED_FOR_IP',
+    'HTTP_PROXY_CONNECTION'
+);
+foreach ($proxy_headers as $x) {
+    if (isset($_SERVER[$x]))
+        die("You are using a proxy!");
+}
 ?>
