b6f84a82098271f6a5f05d58d864a8cd934e1502
hiren <hda@narola.email>
2017-06-19 10:44:34 +0530

    Debugging

--- application/views/video/adcash.php
+++ application/views/video/adcash.php
@@ -6,7 +6,7 @@
 <script src="<?php echo S3_URL; ?>/assets/js/videojs.ads.js"></script>
 <script src="<?php echo S3_URL; ?>/assets/js/vast-client.js"></script>
 <script src="<?php echo S3_URL; ?>/assets/js/videojs.vast.js"></script>
-<video id="vid1" class="video-js vjs-default-skin" autoplay controls preload="auto" width="100%" height="768" poster="<?php echo S3_URL; ?>/Videos/Commercials/luvr-logo.jpg" data-setup="{}">
+<video id="vid1" class="video-js vjs-default-skin" loop autoplay controls preload="auto" width="100%" height="768" poster="<?php echo S3_URL; ?>/Videos/Commercials/luvr-logo.jpg" data-setup="{}">
     <source src="<?php echo S3_URL; ?>/Videos/Commercials/luvr-logo.mp4" type='video/mp4'>
     <p class="vjs-no-js">
         To view this video please enable JavaScript, and consider upgrading to a web browser that
@@ -18,6 +18,6 @@
     vid1.muted(true);
     vid1.ads();
     vid1.vast({
-        url: '<?php echo S3_URL; ?>/assets/lib/sample-vast.xml'
+        url: 'http://playertest.longtailvideo.com/vast-30s-ad.xml'
     });
 </script>
