13abbd92e9565dd08aa82fb14090e054fd1d68e0
hiren <hda@narola.email>
2017-07-05 14:33:16 +0530

    Debugging

---
+++ application/controllers/test.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model(array('Users_model', 'Filters_model', 'Bio_model', 'Videos_model'));
+    }
+
+    public function index() {
+        
+    }
+
+    function test() {
+        pr($_SERVER);
+    }
+
+}
+
+?>
