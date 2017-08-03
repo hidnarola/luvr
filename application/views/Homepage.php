ecb123e8f7f8e3e360868d0e4550998d3b787c3d
hiren <hda@narola.email>
2017-08-03 10:25:46 +0530

    Debugging

--- application/views/Homepage.php
+++ application/views/Homepage.php
@@ -1,4 +1,10 @@
 <?php
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Headers: *, X-Requested-With, Content-Type");
+    header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
+}
 $user_data = $this->session->userdata('user');
 $error = $this->session->flashdata('error');
 $playlist[0] = array("file" => ASSETS_URL . "/Videos/Commercials/vid1.mp4", "image" => ASSETS_URL . "/Videos/Commercials/vid1.jpg");
