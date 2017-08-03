04bee6d2ebefdf0f8a7b5bd7d2b2f2fdcc5be80e
hiren <hda@narola.email>
2017-08-03 10:27:54 +0530

    Debugging

--- application/views/Homepage.php
+++ application/views/Homepage.php
@@ -1,5 +1,6 @@
 <?php
 if (isset($_SERVER['HTTP_ORIGIN'])) {
+    pr($_SERVER);
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
     header("Access-Control-Allow-Headers: *, X-Requested-With, Content-Type");
