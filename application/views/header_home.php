2ff6cebf58bdb31f7869269c4a66f698c072965d
hiren <hda@narola.email>
2017-08-04 12:46:05 +0530

    Debugging

--- application/views/header_home.php
+++ application/views/header_home.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization");
+header("Access-Control-Allow-Headers: *");
 if (!empty($meta_title) && $meta_title != null)
     $site_title = $meta_title;
 else
