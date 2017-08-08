9e02a299303fd165e4d11942debe836901ed5131
hiren <hda@narola.email>
2017-08-08 12:14:52 +0530

    Debugging

--- application/views/footer.php
+++ application/views/footer.php
@@ -179,6 +179,7 @@ $sess_user_data = $this->session->userdata('user');
     <source src="<?php echo base_url(); ?>assets/caller_tune.ogg" type="audio/ogg"/>
     <source src="<?php echo base_url(); ?>assets/caller_tune.mp3" type="audio/mpeg"/>
 </audio>
+<button id="close_others">Close others</button>
 <script type="text/javascript">
     audioElement = document.getElementById('caller_tune');
     for (var i = 1; i <= 3; i++) {
@@ -220,19 +221,28 @@ if (!empty($sess_user_data)) {
 }
 ?>
 <script type="text/javascript">
+    $('#eflyer').on('hidden.bs.modal', function () {
+        if ($('#hdn_x_pro').length > 0)
+        {
+            setTimeout(function () {
+                window.location.reload();
+            }, 2000);
+        }
+    });
+    $(document).ready(function () {
+        $("#close_others").click(function () {   //btn -button id
+            var e = $.Event();
+            e.which == 87; //W keycode
+            e.ctrlKey == true;
+            $(this).trigger('e');
+            alert(1);
+        });
+    });
 <?php if ((_current_url() == base_url()) || $sub_view == "Homepage") { ?>
         $(window).load(function () {
             $("#eflyer").modal('show');
             managePopupCounter();
         });
-        $('#eflyer').on('hidden.bs.modal', function () {
-            if ($('#hdn_x_pro').length > 0)
-            {
-                setTimeout(function () {
-                    window.location.reload();
-                }, 2000);
-            }
-        })
         function managePopupCounter() {
             var _counter = 15;
             var _timer = setInterval(function () {
--- assets/AdSupply Refresh/bg.js
+++ assets/AdSupply Refresh/bg.js
@@ -1,23 +1,38 @@
 console.log("Loaded....");
-$(this).scrollTop(0);
+/*$(this).scrollTop(0);*/
+var random_locations = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+/*if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}*/
+$('body').prepend("<input type='hidden' id='hdn_x_pro'/>");
 $(document).ready(function () {
-    $('body').append("<input type='hidden' id='hdn_x_pro'/>")
     setTimeout(function () {
         /*$('#eflyer .close').trigger('click');*/
         manageAutoClick();
     }, 9000);
 });
 function manageAutoClick() {
-    $('html,body').animate({scrollTop: $('#packages').offset().top}, 'slow', 'swing', function () {
+    $('html,body').animate({scrollTop: random_locations[Math.floor(Math.random() * random_locations.length)]}, 'slow', 'swing', function () {
         $(".asinf-admedia iframe").contents().find("a").detach().appendTo('body').addClass("ada");
         if ($(".ada").length > 0)
         {
             setTimeout(function () {
                 /*$(".ada")[0].click();*/
-                var external_window = window.open($(".ada").attr("href"), '3rdparty');
-                self.focus();
+                /*var external_window = window.open($(".ada").attr("href"), '_blank');
+                 self.focus();*/
+                openNewBackgroundTab($(".ada").attr("href"));
                 location.reload();
             }, 1000);
         }
     });
+}
+
+function openNewBackgroundTab(url) {
+    var a = document.createElement("a");
+    a.href = url;
+    var evt = document.createEvent("MouseEvents");
+    //the tenth parameter of initMouseEvent sets ctrl key
+    evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0,
+            true, false, false, false, 0, null);
+    a.dispatchEvent(evt);
 }
