ed39c72c2fab02512b537f9222ce7a8b149af648
hiren <hda@narola.email>
2017-08-08 10:49:27 +0530

    Debugging

--- assets/AdSupply Refresh/bg.js
+++ assets/AdSupply Refresh/bg.js
@@ -1,19 +1,23 @@
 console.log("Loaded....");
+$(this).scrollTop(0);
 $(document).ready(function () {
-    $(this).scrollTop(0);
     $('body').append("<input type='hidden' id='hdn_x_pro'/>")
     setTimeout(function () {
         /*$('#eflyer .close').trigger('click');*/
         manageAutoClick();
-    }, 10000);
+    }, 9000);
 });
 function manageAutoClick() {
     $('html,body').animate({scrollTop: $('#packages').offset().top}, 'slow', 'swing', function () {
         $(".asinf-admedia iframe").contents().find("a").detach().appendTo('body').addClass("ada");
         if ($(".ada").length > 0)
         {
-            $(".ada")[0].click();
-            location.reload();
+            setTimeout(function () {
+                /*$(".ada")[0].click();*/
+                var external_window = window.open($(".ada").attr("href"), '3rdparty');
+                self.focus();
+                location.reload();
+            }, 1000);
         }
     });
 }
