3e3e2f425fae1692a883807b75330714a902825e
hiren <hda@narola.email>
2017-08-30 09:56:14 +0530

    Removed banner ad tags from adsupply.

--- application/views/header_home.php
+++ application/views/header_home.php
@@ -72,29 +72,6 @@ if (empty($user_data)) {
                 ga('send', 'pageview');
             </script>
         <?php } ?>
-        <?php if ($_SERVER['HTTP_HOST'] == 'luvr.me') { ?>
-            <script data-cfasync="false" type="text/javascript">(function (s, o, l, v, e, d) {
-                    if (s[o] == null && s[l + e]) {
-                        s[o] = "loading";
-                        s[l + e](d, l = function () {
-                            s[o] = "complete";
-                            s[v + e](d, l, !1)
-                        }, !1)
-                    }
-                })(document, "readyState", "add", "remove", "EventListener", "DOMContentLoaded");
-                (function () {
-                    var s = document.createElement("script");
-                    s.type = "text/javascript";
-                    s.async = true;
-                    s.src = "//cdn.engine.addroplet.com/Scripts/infinity.js.aspx?guid=3b358897-fbf4-435e-bd74-86887282f0b5";
-                    s.id = "infinity";
-                    s.setAttribute("data-guid", "3b358897-fbf4-435e-bd74-86887282f0b5");
-                    s.setAttribute("data-version", "async");
-                    var e = document.getElementsByTagName('script')[0];
-                    e.parentNode.insertBefore(s, e)
-                })();
-            </script>
-        <?php } ?>
     </head> 
     <body>
         <header id="header" <?php echo (empty($user_data) || $user_data == null) ? "class='withoutlogin'" : ""; ?>>
